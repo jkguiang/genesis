a66df9b28cedbbc10d6d69d9298f7f9affd02145
Jonathan Guiang <jkguiang@gmail.com>
2022-06-29 09:43:27 -0700

    added ufo model

---
+++ madgraph/models/HHVBF_UFO/coupling_orders.py
@@ -0,0 +1,28 @@
+# This file was automatically created by FeynRules 2.0.5
+# Mathematica version: 9.0 for Mac OS X x86 (64-bit) (January 24, 2013)
+# Date: Mon 25 Nov 2013 08:55:32
+
+
+from object_library import all_orders, CouplingOrder
+
+
+C2V = CouplingOrder(name = 'C2V',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+CV = CouplingOrder(name = 'CV',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+C3 = CouplingOrder(name = 'C3',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
